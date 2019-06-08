c5044e506fa44504eac34bd74955b22e343524f8
Paul Franco <paulfranco@me.com>
2019-06-07 22:35:28 -0700

    updated ListViewModel

--- app/src/main/java/co/paulfran/paulfranco/countries/viewModel/ListViewModel.kt
+++ app/src/main/java/co/paulfran/paulfranco/countries/viewModel/ListViewModel.kt
@@ -1,8 +1,39 @@
 package co.paulfran.paulfranco.countries.viewModel
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import co.paulfran.paulfranco.countries.model.Country
 
 class ListViewModel: ViewModel() {
 
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(Country("CountryA"),
+                Country("CountryB"),
+                Country("CountryC"),
+                Country("CountryD"),
+                Country("CountryE"),
+                Country("CountryF"),
+                Country("CountryG"),
+                Country("CountryH"),
+                Country("CountryI"),
+                Country("CountryJ"),
+                Country("CountryK"),
+                Country("CountryL"),
+                Country("CountryM"),
+                Country("CountryN")
+        )
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+
+    }
 
 }
